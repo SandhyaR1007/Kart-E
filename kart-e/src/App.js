be90d9f7af0998a79a0862d3169362364b121d68
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-05-03 23:28:29 +0530

    initialized project and added starter packages

---
+++ kart-e/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+      hello there
+    </div>
+  );
+};
+
+export default App;
