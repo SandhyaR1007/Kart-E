23ca5812fc6889b978db22a1e91ed47f2bc00c7d
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-05-15 17:49:09 +0530

    added ui library

--- kart-e/src/App.js
+++ kart-e/src/App.js
@@ -1,9 +1,8 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return (
-    <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-      hello there
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
